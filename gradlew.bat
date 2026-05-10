0bff0f7577f152cbcd249d114fdbec4dc00924a3
smsjs <smsjsodj@gmail.com>
2026-05-10 12:32:42 +0200

    add gradle wrapper files

--- gradlew.bat
+++ gradlew.bat
@@ -1,16 +1,7 @@
 @echo off
-setlocal
-
 set DIRNAME=%~dp0
-if "%DIRNAME%"=="" set DIRNAME=.
-
-set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%
-
-set DEFAULT_JVM_OPTS=
-
+set JAVA_EXE=java
+set DEFAULT_JVM_OPTS=-Xmx64m -Xms64m
 set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
-
-java %DEFAULT_JVM_OPTS% -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
-
-endlocal
+"%JAVA_EXE%" %DEFAULT_JVM_OPTS% -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
